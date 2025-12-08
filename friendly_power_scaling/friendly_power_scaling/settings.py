40b940f891cb7d154d66e9e6532aa6000090e1e0
JosiahErnst <josiah.ernst.2003@gmail.com>
2025-12-08 09:57:06 -0600

    Fixed CSS issue

--- friendly_power_scaling/friendly_power_scaling/settings.py
+++ friendly_power_scaling/friendly_power_scaling/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Added from django docs
+STATICFILES_DIRS = [
+    BASE_DIR / 'FriendlyPowerScaling/static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
